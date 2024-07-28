86e9f3ec1a42434b14d830209fd6290dbe4f9c7e
armand <aanger3@gmail.com>
2024-07-28 13:13:05 +0200

    mariaDB ok

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -3,16 +3,32 @@
 
 #variable d'environement a rajouter dans le .env
 
-# SQL_USER="aranger42"
-# SQL_PASSWORD="0000"
-# SQL_DATABASE="newdb"
-# SQL_ROOT_PASSWORD="ademine"
+SQL_USER="aranger42"
+SQL_PASSWORD="0000"
+SQL_DATABASE="newdb"
+SQL_ROOT_PASSWORD="ademine"
 
 service mariadb start
-mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" #remplacer newdb par $VAR
-mysql -e "CREATE USER IF NOT EXISTS $SQL_USER IDENTIFIED BY $SQL_PASSWORD;"
-mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER IDENTIFIED BY $SQL_PASSWORD;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY $SQL_ROOT_PASSWORD;"
-mysql -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+sleep 10
+
+# Créer la base de données si elle n'existe pas déjà
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# Créer un utilisateur si il n'existe pas déjà et lui attribuer un mot de passe
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+
+# Accorder tous les privilèges sur la base de données à l'utilisateur
+mysql -e "GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+
+# Modifier le mot de passe de l'utilisateur root
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
+
+# Rafraîchir les privilèges pour prendre en compte les modifications
+mysql -u root -p"$SQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+
+# Arrêter le service mysql
+mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown
+
+# Démarrer mysql en mode sécurisé
 exec mysqld_safe
