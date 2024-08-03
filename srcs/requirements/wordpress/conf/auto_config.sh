4ce92f2d9fe0a6ede0c84bf264b533ea81942922
aranger <aanger3@gmail.com>
2024-08-03 12:57:06 +0200

    ok

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 sleep 10
-wp config create --allow-root \
-				--dbname=$SQL_DATABASE \
-				--dbuser=$SQL_USER \
-				--dbpass=$SQL_PASSWORD \
-				--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+cd /var/www/wordpress
+
+if ! [ -e wp-config.php ]; then
+	wp core downlo
+	wp config create --allow-root \
+					--dbname=$SQL_DATABASE \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+
+wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
