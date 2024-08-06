1d09158e1d33b2df06ef332319108ec8eea52ca3
armand <aanger3@gmail.com>
2024-08-06 15:27:04 +0200

    Mariadb

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -4,13 +4,15 @@ sleep 10
 ping mariadb -c 3
 cd /var/www/wordpress
 
-wp config create --allow-root \
-				--dbname=$SQL_DATABASE \
-				--dbuser=$SQL_USER \
-				--dbpass=$SQL_PASSWORD \
-				--dbhost=mariadb:3306 --path='/var/www/wordpress'
-
-echo "Configuration terminée."
+if [ ! -f wp-config.php ]; then
+  wp config create --allow-root \
+      --dbname=$SQL_DATABASE \
+      --dbuser=$SQL_USER \
+      --dbpass=$SQL_PASSWORD \
+      --dbhost=mariadb:3306 --path='/var/www/wordpress'
+  echo "Configuration terminée."
+else
+  echo "wp-config.php already exists."
+fi
 
 php-fpm7.4 -F
-#wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
