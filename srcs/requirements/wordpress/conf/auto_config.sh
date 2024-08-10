450ecf089d87123c7a30777e6b3e5314759c950f
aranger <aanger3@gmail.com>
2024-08-10 15:16:40 +0200

    ok

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -7,6 +7,7 @@ cd /var/www/wordpress
 if [ ! -f wp-config.php ]; then
   wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
   wp core install --allow-root --url=$DOMAIN_NAME --title="Hello" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --path='/var/www/wordpress'
+  wp user create 
   echo "Configuration terminée."
 else
   echo "wp-config.php already exists."
